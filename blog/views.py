91b0d88ff327cf2e5a64915cb31fbdcfcc4be08c
AJ Bowen <aj@soulshake.net>
2020-05-09 14:55:43 +0200

    Add post detail page

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from blog.models import Post
 from django.utils import timezone
 
@@ -10,3 +10,8 @@ def post_list(request):
     posts = Post.objects.all()
     print(posts)
     return render(request, 'blog/post_list.html', {'posts': posts})
+
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
